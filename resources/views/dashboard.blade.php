5c90617edc86304d654a2d65b573526eca44e89e
ulul363 <ululazmi.stu@pnc.ac.id>
2024-07-12 20:14:59 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,7 +26,7 @@
                     <div class="row align-items-center">
                         <div class="col-md-12">
                             <div class="page-header-title">
-                                <h5 class="m-b-10">Selamat datang <span>{{ Auth::user()->name }}</span></h5>
+                                <h5 class="m-b-10">Selamat Datang <span>{{ Auth::user()->name }}</span></h5>
                             </div>
                             <ul class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="index.html"><i class="feather icon-home"></i></a></li>
