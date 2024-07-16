9bdbe3334151a843fcb21167bb889b16180d7fac
daffaabp <daffa.budi2003@gmail.com>
2024-07-16 11:57:04 +0700

    update tipe data integer

--- database/migrations/2024_07_13_152537_create_program_kegiatan_table.php
+++ database/migrations/2024_07_13_152537_create_program_kegiatan_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->unsignedBigInteger('ketua_id');
             $table->unsignedBigInteger('verifikasi_id')->nullable();
             $table->string('nama_program', 50);
-            $table->int('tahun_ajaran', 4);
+            $table->integer('tahun_ajaran', 4);
             $table->string('deskripsi', 200);
             $table->enum('status', ['pending', 'disetujui', 'ditolak'])->default('pending');
             $table->timestamps();
